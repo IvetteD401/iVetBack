2a8ed590e4ade2dbb6368827c3a9c65a2887be9c
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-27 20:42:40 +0000

    new

--- src/main.py
+++ src/main.py
@@ -7,8 +7,9 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
-#from models import Person
+from models import db , User, Dogfile , GeneralRecords
+from flask_jwt_simple import (JWTManager, jwt_required, create_jwt, get_jwt_identity)
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -18,8 +19,13 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
+
+# Setup the Flask-JWT-Simple extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
+
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
@@ -28,11 +34,97 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/login', methods=['POST'])
+def login():
+    
+    if request.method == 'POST':
+        body = request.get_json()
+        user = User.query.filter_by(email=body['email'], password=body['password']).first()
+
+    if not user:
+        return 'User not found', 404
+    if not user.email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if not user.password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    return jsonify({
+              'token': create_jwt(identity=1),
+              'id': user.id,
+              'email': user.email,
+              'firstName': user.firstName
+              })
+
+@app.route('/new', methods=['POST'])
+def new_user():
+    if request.method == 'POST':
+        body = request.get_json()
+
+        db.session.add(User(
+            firstName = body["firstName"],
+            email = body["email"],
+            password = body['password'],
+            petname = body['petname']
+            # date = body['date']
+        ))
+        
+        db.session.commit()
+        return jsonify({
+            'msg': 'User Added!'
+        })
+
+    # # GET request
+    # if request.method == 'GET':
+    #     all_users = User.query.all()
+    #     all_users = list(map(lambda x: x.serialize(), all_users))
+    #     return jsonify(all_users), 200
+
+    # return "Invalid Method", 404
+
+# Protect a view with jwt_required, which requires a valid jwt
+# to be present in the headers.
+# @app.route('/protected', methods=['GET'])
+# @jwt_required
+# def protected():
+#     # Access the identity of the current user with get_jwt_identity
+#     return jsonify({'hello_from': get_jwt_identity()}), 200
+
+@app.route('/dogfile', methods=['POST', 'GET'])
+def get_dogfile():
+
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if "petname" not in body:
+            raise APIException('You need to specify the petname', status_code=400)
+        if 'dob' not in body:
+            raise APIException('You need to specify the dob', status_code=400)
+        if 'imglink' not in body:
+            body['imglink'] = None
+
+        dog = Dogfile(petname=body['petname'], dob = body['dob'], imglink = body['imglink'])
+        db.session.add(dog)
+        db.session.commit()
+
+        return "ok", 200
+    
+    # GET request
+    if request.method == 'GET':
+        all_dogs = Dogfile.query.all()
+        all_dogs = list(map(lambda x: x.serialize(), all_dogs))
+        return jsonify(all_dogs), 200
+
+    return "Invalid Method", 404
+
+
+
+
 @app.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
     response_body = {
-        "hello": "world"
+        "hello": "oreo"
     }
 
     return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -1,17 +1,82 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    # date = db.Column(db.Integer, unique=False, nullable=False)
+    petname = db.Column(db.String(120),unique=False, nullable=False)
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.firstName
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def serialize(self):
+        return {
+            "firstName": self.firstName,
+            "email": self.email,
+            "password": self.password,
+            "petname": self.petname
+            # "date": self.date
+    }
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return '<Login %r>' % User.firstName
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "firstName": User.firstName,
+            "email": User.email,
+            "password": User.password,
+            "petname": User.petname,
+            # "date": User.date
+    }
+class Dogfile(db.Model):
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    petname = db.Column(db.String(80), unique=False, nullable=False)
+    dob = db.Column(db.String(60), unique=False, nullable=False)
+    imglink = db.Column(db.String(120), unique=False, nullable=False)
+    # user_id = db.Column(db.String(80), db.ForeignKey('user.id'),
+        # nullable=False)
+     
+    def __repr__(self):
+        return '<Dogfile %r>' % self.petname
+
+    def serialize(self):
+        return {
+            "petname": self.petname,
+            "dob": self.dob,
+            "imglink": self.imglink
+            # "user_id": self.user_id
+    }
+
+class GeneralRecords(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    vet_name = db.Column(db.String(80), unique=False, nullable=False)
+    groomer_name = db.Column(db.String(80), unique=False, nullable=False)
+    vet_address = db.Column(db.String(120), unique=False, nullable=False)
+    groomer_address = db.Column(db.String(120), unique=False, nullable=False)
+    insurance_policy = db.Column(db.String(120), unique=True, nullable=False)
+    insurance_provider = db.Column(db.Integer, unique=False, nullable=False)
+    petname = db.Column(db.String(80), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return '<GeneralRecords %r>' % self.petname
+
+    def serialize(self):
+        return {
+            "vet_name": self.vet_name,
+            "groomer_name": self.groomer_name,
+            "vet_address": self.vet_address,
+            "groomer_address": self.groomer_address,
+            "insurance_policy": self.insurance_policy,
+            "insurance_provider": self.insurance_provider,
+            "petname": self.petname,
+    }
